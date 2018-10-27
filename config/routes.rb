8602c9b800bee89388ec8fbd5468c136a339947a
Dofrasilva12 <ulfrsilvagu@ittepic.edu.mx>
2018-10-27 01:38:37 +0000

    int

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :productos
   resources :clientes
   get 'welcome/index'
+  @clientes = clientes.find(params[:clientes])
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
